fbba44085b00b0a160244a45abc91eaf952f31bd
Lucas Waye <wayetender@gmail.com>
2017-05-04 19:19:15 -0400

    Update service.thrift

--- hello-world/assets/service.thrift
+++ hello-world/assets/service.thrift
@@ -1,4 +1,16 @@
-include "clientprotocol.thrift"
+
+namespace cpp ClientProxy
+
+struct RedirectionInfo {
+    1: bool is_proxied;
+    2: string address;
+    3: i32 port;
+}
+
+service Redirection {
+    RedirectionInfo get_redirection_info(1: string address, 2: i32 port);
+}
+
 
 typedef i32 int
 typedef i64 long
